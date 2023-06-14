bc1e781a9db5613cf5ad81a6125e042330dc4af3
Winsor Tse <tsewinsor@gmail.com>
2023-06-14 01:55:18 -0400

    adding stuff

--- Main.py
+++ Main.py
@@ -1,3 +1,27 @@
 
+#mapping of every type
+#hashmap
+
+
 if __name__ == '__main__':
-    print(hello)
+    types = ['Normal',
+            'Fire',
+            'Water',
+            'Grass',
+            'Electric',
+            'Ice',
+            'Fighting',
+            'Poison',
+            'Ground',
+            'Flying',
+            'Psychic',
+            'Bug',
+            'Rock',
+            'Ghost',
+            'Dark',
+            'Dragon',
+            'Steel',
+            'Fairy']
+    for t in types:
+        print(t)
+        att_type[t] = 
